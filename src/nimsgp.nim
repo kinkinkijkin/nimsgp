1ff0a65bfa43056c54a29eb7259991c3575b9101
Averey Vermette <soldiertheinsanitywarrior@gmail.com>
2022-03-20 18:23:03 -0400

    rectify exports in primary library file

--- src/nimsgp.nim
+++ src/nimsgp.nim
@@ -1,5 +1,5 @@
 import glm
 
-import nimsgp/[datatypes,pixelchoose,topdraw,toptransform]
+import nimsgp/[datatypes,pixelchoose,topdraw,toptransform,funcs2d]
 
-export glm, datatypes, pixelchoose, topdraw, toptransform
+export glm, datatypes, pixelchoose, topdraw, toptransform, funcs2d
